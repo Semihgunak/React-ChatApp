77e5fca297073d75c9c0e763de6b3a50aaaa9e18
Semih Günak <semihgunak@gmail.com>
2024-04-23 14:15:37 +0300

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5000", {
+			const socket = io("https://react-chatapp-wlx6.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
